fcbd919969482aac76df9dd7b14bc470610ba319
Nguyễn Bá Tuấn Anh <anh.nguyen16@codegym.vn>
2025-02-20 20:11:41 +0700

    Update Lesson 06 lifecycle method

--- lesson04-react-state-and-lifecycle-method/src/components/SignInForm.js
+++ lesson04-react-state-and-lifecycle-method/src/components/SignInForm.js
@@ -1,36 +1,88 @@
-import { Component } from "react";
-import "./SignInForm.css";
+import { Component } from 'react';
+import './SignInForm.css';
 
 class SignInForm extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      username: "",
-      password: "",
+      username: '',
+      password: '',
+      error: '',
+      isLoggedIn: false,
     };
 
     // Ràng buộc this cho các hàm (phương thức) khi sử dụng this
     this.handleSubmit = this.handleSubmit.bind(this);
+
+    // Initialization: Giai đoạn khởi tạo
+    console.log(
+      'Component SigninForm khởi tạo các giá trị ban đầu cho state hoặc props đã cho'
+    );
   }
 
-  // Arrow function
-  // handleSubmit(event) {
-  //   event.preventDefault();
-  //   console.log(this);
-  //   console.log(this.state);
-  // }
+  // Giai đoạn Mouting
+  componentDidMount() {
+    document.title = 'Login Page';
+    // TODO: Gọi API ở đây
+    console.log(': Được gọi ngay sau khi giai đoạn khởi tạo hoàn thành!');
+  }
 
-  handleSubmit = (event) => {
+  shouldComponentUpdate() {
+    return true;
+  }
+
+  // Giai đoạn Updating
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevState.username !== this.state.username ||
+      prevState.password !== this.state.password
+    ) {
+      if (this.state.username.includes('$')) {
+        this.setState({
+          error: 'Định dạng username không đúng! (Chỉ chứa kí tự [a-zA-Z0-9])',
+        });
+      }
+      console.log('Hàm componentDidUpdate được gọi!');
+    }
+  }
+
+  // Giai đoạn Unmouting
+  componentWillUnmount() {
+    console.log('Hàm componentWillUnmount được gọi!');
+    document.title = 'React App';
+  }
+
+  handleSubmit(event) {
     event.preventDefault();
-    console.log(this.state);
+    if (this.state.username === 'admin' && this.state.password === 'admin') {
+      this.setState({ isLoggedIn: true });
+    } else {
+      this.setState({ error: 'Sai thông tin đăng nhập!' });
+    }
+  }
+
+  // // Arrow function
+  // handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   console.log(this.state);
+  // };
+
+  logout = () => {
+    this.setState({ isLoggedIn: false, username: '', password: '' });
   };
 
   render() {
-    return (
+    console.log('Hàm render() được gọi');
+    return this.state.isLoggedIn ? (
+      <button onClick={() => this.logout()}>Logout</button>
+    ) : (
       <div className="container">
         <form onSubmit={this.handleSubmit}>
           <h1>Login</h1>
+          {this.state.error && (
+            <p style={{ color: 'red' }}>{this.state.error}</p>
+          )}
           <input
             type="text"
             placeholder="Username"
--- lesson04-react-state-and-lifecycle-method/src/index.js
+++ lesson04-react-state-and-lifecycle-method/src/index.js
@@ -1,8 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/App";
-import SignInForm from "./components/SignInForm";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+import SignInForm from './components/SignInForm';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
-// root.render(<SignInForm />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showLoginForm: false,
+    };
+  }
+
+  toggleShowLoginForm = () => {
+    this.setState({ showLoginForm: !this.state.showLoginForm });
+  };
+
+  render() {
+    return (
+      <>
+        {this.state.showLoginForm && <SignInForm />}
+        <button onClick={() => this.toggleShowLoginForm()}>
+          Ẩn/Hiện Login
+        </button>
+      </>
+    );
+  }
+}
+
+// root.render(<App />);
+root.render(<Home />);
